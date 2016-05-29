6eadbb95a4fae3d9f2205950e30d7d0eb24b1c2c
llamatarianism <billy.korbey.wright@gmail.com>
2016-05-29 23:22:06 +0100

    Initial commit

---
+++ test/url_shortener/handler_test.clj
@@ -0,0 +1,14 @@
+(ns url-shortener.handler-test
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [url-shortener.handler :refer :all]))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (mock/request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+
+  (testing "not-found route"
+    (let [response (app (mock/request :get "/invalid"))]
+      (is (= (:status response) 404)))))
